931d007f48eb3f75ea703a67b3ee758b069b6158
AriefHariss <150606921+AriefHariss@users.noreply.github.com>
2023-11-12 22:49:11 +0700

    Add files via upload

---
+++ ResponsiPBO/desturctor.php
@@ -0,0 +1,11 @@
+<?php
+
+// import data/person.php
+require_once "data/Person.php";
+
+// buat 2 object new person dengan parameter yang berbeda
+$arff = new Person("Arief", "Bengkulu");
+$prass = new Person("Prasetyo", "Jogja");
+
+// tambahkan echo "Program Selesai" . PHP_EOL;
+echo "Program Selesai" . PHP_EOL;
